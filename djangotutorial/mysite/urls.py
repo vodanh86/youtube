133ba13d34219b1ec4d32e93f024d27dbe7c6f6b
cuongdm <cuongdm172@gmail.com>
2025-03-20 22:25:01 +0700

    add page

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from pages.views import home_view
 from django.urls import include, path
 
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('home/', home_view, name='home'),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
