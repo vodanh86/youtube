b59c1711e3f99298d920206ed8187c7fd32b19eb
cuongdm <cuongdm172@gmail.com>
2025-03-22 06:18:47 +0700

    add download result

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from pages.views import home_view
+from pages.views import home_view, url_view
 from django.urls import include, path
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
+    path('post/url', url_view, name = "post_url"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
--- djangotutorial/pages/views.py
+++ djangotutorial/pages/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from yt_dlp import YoutubeDL
 
 # Create your views here.
 def home_view(request,*args, **kwargs):
     return render(request, "pages/index.html", {})
+
+# Create your views here.
+def url_view(request,*args, **kwargs):
+    url = request.POST['url']
+    ydl_opts = {
+        'playlist_items': '1',
+        'extract_flat': 'in_playlist',
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+    print(info.get('title'))
+    infoFile = open("info.json", "w")
+    infoFile.write(str(info))
+    infoFile.close()
+    return render(request, "pages/result.html", {"info": info})
