084d9683e3629518146fc216108b75e5353b5fa1
cuongdm <cuongdm172@gmail.com>
2025-03-24 14:58:24 +0700

    update image

--- djangotutorial/pages/views.py
+++ djangotutorial/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from yt_dlp import YoutubeDL, utils
-
+import validators
 # Create your views here.
 
 
@@ -14,31 +14,43 @@ def home_view(request, *args, **kwargs):
 def url_view(request, *args, **kwargs):
     url = request.POST['url']
     ydl_opts = {
+        "default-search": "ytsearch",
     }
 
     with YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-        print(info.get('title'))
-        '''infoFile = open("info.json", "w")
-        infoFile.write(str(info.get('formats')))
-        infoFile.close()'''
-        formats = info.get('formats')
-        audio = ""
-        videos = []
-        for format in formats:
-            new_format = [format.get('ext'),
-                          format.get('filesize'),
-                          utils.format_bytes(format.get('filesize') if format.get('filesize') is not None else format.get('filesize_approx')),
-                          format.get('url'),
-                          format.get('acodec'),
-                          format.get('vcodec'),
-                          format.get('resolution'),
-                          format.get('height')]
-            if format.get('ext') == 'm4a':
-                audio = new_format
-            if format.get('ext') == 'mp4' and format.get('vcodec').find("audio") == -1:
-                videos.append(new_format)
+        if validators.url(url):
+            info_videos = [ydl.extract_info(url, download=False)]
+        else:
+            info_videos = ydl.extract_info(f"ytsearch:{url}", download=False).get('entries')
 
-        print(audio)
-        print(videos)
-    return render(request, "pages/result.html", {"info": info})
+        updated_info = []
+        for info in info_videos:
+            formats = info.get('formats')
+            audio = []
+            video = {}
+            videos = {}
+            for format in formats:
+                new_format = [format.get('ext'),
+                            format.get('filesize'),
+                            utils.format_bytes(format.get('filesize') if format.get('filesize') is not None else format.get('filesize_approx')),
+                            format.get('url'),
+                            format.get('acodec'),
+                            format.get('vcodec'),
+                            format.get('resolution'),
+                            format.get('height')]
+                if format.get('ext') == 'm4a':
+                    audio = new_format
+                if format.get('ext') == 'mp4' and format.get('resolution').find("audio") == -1:
+                    print(format.get('acodec'))
+                    if format.get('acodec') != "none":
+                        video = new_format
+                    else:
+                        list_video = videos.get(format.get('height'), [])
+                        list_video.append(new_format)
+                        videos[format.get('height')] = list_video
+            updated_info.append((info, audio, video, videos))
+    f = open("infor.json", "w")
+    f.write(str(updated_info))
+    f.close()
+    print(updated_info)
+    return render(request, "pages/result.html", {"updated_info": updated_info})
