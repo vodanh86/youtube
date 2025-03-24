b05acc192a228cc3dd0940b442fd0ff370640f5b
cuongdm <cuongdm172@gmail.com>
2025-03-24 23:43:03 +0700

    update list videos

--- djangotutorial/pages/models.py
+++ djangotutorial/pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    videoId = models.CharField(max_length=200)
+    link = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    thumbnail = models.CharField(max_length=1000)
--- djangotutorial/pages/views.py
+++ djangotutorial/pages/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from yt_dlp import YoutubeDL, utils
+from .models import Video
 import validators
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/index.html", {})
+    # Lấy tất cả video từ cơ sở dữ liệu
+    videos = Video.objects.all()
+
+    # Lấy 5 video cuối cùng
+    last_5_videos = videos.order_by('-id')[:5]
+
+    return render(request, "pages/index.html", {"videos": last_5_videos})
 
 # Create your views here.
 
@@ -21,7 +28,8 @@ def url_view(request, *args, **kwargs):
         if validators.url(url):
             info_videos = [ydl.extract_info(url, download=False)]
         else:
-            info_videos = ydl.extract_info(f"ytsearch:{url}", download=False).get('entries')
+            info_videos = ydl.extract_info(
+                f"ytsearch:{url}", download=False).get('entries')
 
         updated_info = []
         for info in info_videos:
@@ -31,23 +39,36 @@ def url_view(request, *args, **kwargs):
             videos = {}
             for format in formats:
                 new_format = [format.get('ext'),
-                            format.get('filesize'),
-                            utils.format_bytes(format.get('filesize') if format.get('filesize') is not None else format.get('filesize_approx')),
-                            format.get('url'),
-                            format.get('acodec'),
-                            format.get('vcodec'),
-                            format.get('resolution'),
-                            format.get('height')]
+                              format.get('filesize'),
+                              utils.format_bytes(format.get('filesize') if format.get(
+                                  'filesize') is not None else format.get('filesize_approx')),
+                              format.get('url'),
+                              format.get('acodec'),
+                              format.get('vcodec'),
+                              format.get('resolution'),
+                              format.get('height')]
                 if format.get('ext') == 'm4a':
                     audio = new_format
                 if format.get('ext') == 'mp4' and format.get('resolution').find("audio") == -1 and format.get("url").find("manifest") == -1:
                     print(format.get('acodec'))
                     if format.get('acodec') != "none":
+                        print(new_format)
                         video = new_format
                     else:
                         list_video = videos.get(format.get('height'), [])
                         list_video.append(new_format)
                         videos[format.get('height')] = list_video
             updated_info.append((info, audio, video, videos))
-    print(updated_info)
+
+        # add into db
+        info = updated_info[0][0]
+        # check id video exist
+        if not Video.objects.filter(videoId=info.get('id')).exists():
+            video_obj = Video(videoId=info.get('id'),
+                                link=info.get('webpage_url'),
+                                title=info.get('title'),
+                                description=info.get('description'),
+                                thumbnail=info.get('thumbnail'))
+            video_obj.save()
+
     return render(request, "pages/result.html", {"updated_info": updated_info})
